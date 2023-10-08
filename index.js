6ca3ad67f82006a944b868679277522f0764f03d
Hitman <hitman264212118@gmail.com>
2023-10-08 23:05:46 +0530

    Rate limitting done for particular IP

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const { rateLimit } = require('express-rate-limit');
 
 
 const  {createProxyMiddleware} = require('http-proxy-middleware');
@@ -7,8 +8,13 @@ const app = express();
 
 const PORT = 3005;
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 5, // Limit each IP to 4 requests per `window` (here, per 15 minutes)
+})
 
 app.use(morgan('combined')); // for logging 
+app.use(limiter);
 
 app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:6005/'}));
 
